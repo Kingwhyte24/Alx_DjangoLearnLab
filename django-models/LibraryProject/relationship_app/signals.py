88842446b708e88b14fecb69e4969cc9b1313da3
Kingwhyte24 <ohagbanusikingsley@gmail.com>
2025-03-19 18:38:36 +0100

    Implement Role-Based Access Control in Django

--- django-models/LibraryProject/relationship_app/signals.py
+++ django-models/LibraryProject/relationship_app/signals.py
@@ -1,15 +1,13 @@
-from django.db import models
+from django.db.models.signals import post_save
+from django.dispatch import receiver
 from django.contrib.auth.models import User
+from .models import UserProfile  # Import, DON'T redefine
 
-class UserProfile(models.Model):
-    ROLE_CHOICES = [
-        ('Admin', 'Admin'),
-        ('Librarian', 'Librarian'),
-        ('Member', 'Member'),
-    ]
-    
-    user = models.OneToOneField(User, on_delete=models.CASCADE)
-    role = models.CharField(max_length=10, choices=ROLE_CHOICES)
+@receiver(post_save, sender=User)
+def create_user_profile(sender, instance, created, **kwargs):
+    if created:
+        UserProfile.objects.create(user=instance)
 
-    def __str__(self):
-        return f"{self.user.username} - {self.role}"
+@receiver(post_save, sender=User)
+def save_user_profile(sender, instance, **kwargs):
+    instance.userprofile.save()
