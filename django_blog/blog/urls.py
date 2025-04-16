cc593a076a7d2d929e7a87dd57ab3c6b9ce1e1f3
Kingwhyte24 <ohagbanusikingsley@gmail.com>
2025-04-16 10:38:05 +0100

    Creating Blog Post Management Features

--- django_blog/blog/urls.py
+++ django_blog/blog/urls.py
@@ -1,11 +1,17 @@
 from django.urls import path
 from . import views
 from django.contrib.auth import views as auth_views
+# from .views import PostListView, PostCreateView, PostDeleteView, PostDetailView, PostUpdateView
 
 urlpatterns = [
     path('register/', views.register_view, name='register'),
     path('login/', auth_views.LoginView.as_view(template_name='blog/login.html'), name='login'),
     path('logout/', auth_views.LogoutView.as_view(template_name='blog/logout.html'), name='logout'),
-    path('profile/', views.profile_view, name='profile')
+    path('profile/', views.profile_view, name='profile'),
+    path('posts/', views.PostListView.as_view(), name='post-list'),
+    path('posts/', views.PostCreateView.as_view(), name='post-create'),
+    path('posts/', views.PostCreateView.as_view(), name='post-detail'),
+    path('posts/', views.PostUpdateView.as_view(), name='post-update'),
+    path('posts/', views.PostDeleteView.as_view(), name='post-delete'),
 
 ]
--- django_blog/blog/views.py
+++ django_blog/blog/views.py
@@ -2,6 +2,10 @@ from django.shortcuts import render, redirect
 from .forms import UserRegisterForm
 from django.contrib.auth import login
 from django.contrib.auth.decorators import login_required
+from django.views.generic import ListView, DeleteView, DetailView, CreateView, UpdateView
+from .models import Post
+from django.contrib.auth.mixins import LoginRequiredMixin, UserPassesTestMixin
+from django.urls import reverse_lazy
 
 # Create your views here.
 
@@ -20,3 +24,44 @@ def register_view(request):
 @login_required
 def profile_view(request):
     render(request, 'blog/profile.html')
+
+
+class PostListView(ListView):
+    model = Post
+    template_name = 'blog/post_list.html'
+    context_object_name = 'posts'
+    ordering = ['-published_date']
+
+class PostDetailView(DetailView):
+    model = Post
+    template_name = 'blog/detail.html'
+
+class PostCreateView(LoginRequiredMixin, CreateView):
+    model = Post
+    fields = ['title', 'content']
+    template_name = 'blog/post_form.html'
+
+    def form_valid(self, form):
+        form.instance.author = self.request.user
+        return super().form_valid(form)
+class PostUpdateView(UpdateView, LoginRequiredMixin, UserPassesTestMixin):
+    model = Post
+    template_name = 'blog/post_form.html'
+    fields = ['title', 'content']
+
+    def form_valid(self, form):
+        form.instance.author = self.request.user
+        return super().form_valid(form)
+    
+    def test_func(self):
+        post = self.get_object()
+        return self.request.user == post.author
+    
+class PostDeleteView(DeleteView, UserPassesTestMixin, LoginRequiredMixin):
+    model = Post
+    template_name = 'blog/post_confirm_delete.html'
+    success_url = reverse_lazy('post-list')
+
+    def test_func(self):
+        post = self.get_object()
+        return self.request.user == post.author
