68d2b1355b26064a16a2aa7afbc7379ea26090f7
Kingwhyte24 <ohagbanusikingsley@gmail.com>
2025-04-16 10:45:14 +0100

    Creating Blog Post Management Features (fixed urls)

--- django_blog/blog/urls.py
+++ django_blog/blog/urls.py
@@ -9,9 +9,9 @@ urlpatterns = [
     path('logout/', auth_views.LogoutView.as_view(template_name='blog/logout.html'), name='logout'),
     path('profile/', views.profile_view, name='profile'),
     path('posts/', views.PostListView.as_view(), name='post-list'),
-    path('posts/new/', views.PostCreateView.as_view(), name='post-create'),
-    path('posts/<int:pk>/', views.PostCreateView.as_view(), name='post-detail'),
-    path('posts/<int:pk>/update', views.PostUpdateView.as_view(), name='post-update'),
-    path('posts/<int:pk>/delete', views.PostDeleteView.as_view(), name='post-delete'),
+    path('post/new/', views.PostCreateView.as_view(), name='post-create'),
+    path('post/<int:pk>/', views.PostCreateView.as_view(), name='post-detail'),
+    path('post/<int:pk>/update', views.PostUpdateView.as_view(), name='post-update'),
+    path('post/<int:pk>/delete', views.PostDeleteView.as_view(), name='post-delete'),
 
 ]
